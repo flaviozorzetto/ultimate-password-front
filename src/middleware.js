7597f80d786056983854155314856671f9f7a8ba
flaviozorzetto <flaviozorzetto11@gmail.com>
2023-10-02 17:29:00 -0300

    middleware update open

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
  
 export function middleware(request) {
-    if (!request.cookies.has('ultimatepassword_token'))
-        return NextResponse.redirect(new URL('/login', request.url))
+    // if (!request.cookies.has('ultimatepassword_token'))
+    //     return NextResponse.redirect(new URL('/login', request.url))
 }
  
 export const config = {
